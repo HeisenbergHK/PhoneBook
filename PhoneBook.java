ceed26740738a79a89234137a238365d258eccc3
Akbar <ahbarurmia@gmail.com>
2022-09-12 21:41:30 +0430

    "Added class: Contact"

--- PhoneBook.java
+++ PhoneBook.java
@@ -17,5 +17,6 @@ public class PhoneBook {
 
         return result;
     }
+
 }
 
---
+++ data/Contact.java
@@ -0,0 +1,10 @@
+public class Contact {
+
+    private String name;
+    private int number;
+
+    public Contact (String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+}
