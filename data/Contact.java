b39079030c6c1f65c5346a51859263fecdd0e693
Akbar <ahbarurmia@gmail.com>
2022-09-15 00:15:06 +0430

    "accessor methods added"

--- data/Contact.java
+++ data/Contact.java
@@ -1,10 +1,23 @@
 public class Contact {
 
     private String name;
-    private int number;
+    private String number;
 
-    public Contact (String name, int number) {
+    public Contact (String name, String number) {
         this.name = name;
         this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String input) {
+        this.name = input;
+    }
+    public String getNumber () {
+        return number;
+    }
+    public void setNumber (String input) {
+        this.number = input;
     }
 }
